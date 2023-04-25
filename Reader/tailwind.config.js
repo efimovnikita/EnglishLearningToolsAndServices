21545d16ef6a65f0ac13c4a06036d046ee870c39
maskedball <maskedballmail@gmail.com>
2023-04-25 16:38:33 +0000

    Continue working with layout

--- Reader/tailwind.config.js
+++ Reader/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./**/*.{razor,html,cshtml}'],
-  theme: {
-    extend: {
-    },
-    plugins: [],
-  }
+  plugins: [
+    require('tailwind-scrollbar')
+  ]
 }
