58b2500c6701c4b47bda4ea62b1ae88169fa5a1c
maskedball <maskedballmail@gmail.com>
2023-05-29 17:36:11 +0300

    Added new project "ParaphraserBot" to the solution file and implemented methods for splitting a Fraction into subfractions based on a given limit. Also added a TikToken field to the Fraction class for encoding and decoding.

---
+++ ParaphraserBot/Program.cs
@@ -0,0 +1,240 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using NAudio.Wave;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using Reader.Models;
+using Reader.Tools;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using TiktokenSharp;
+using File = System.IO.File;
+
+namespace ParaphraserBot;
+
+internal class Program
+{
+    private const int Limit = 500;
+    private const string SystemMessage = "You are professional English teacher. Your speciality is to paraphrase given text into easy and understandable variation for intermediate English learner.";
+    private const string OutputFileName = "merged.wav";
+    private static List<int>? _allowedUsersList;
+    private static string? _openAiApiKey;
+    private static TikToken? _tikToken;
+    private static readonly string AssemblyLocation = Directory.GetCurrentDirectory();
+    private static readonly string PiperFolderPath = Path.Combine(AssemblyLocation, "piper");
+
+    public static void Main(string[] args)
+    {
+        string? telegramBotApiKey = Environment.GetEnvironmentVariable("TELEGRAM_BOT_API_KEY");
+        if (telegramBotApiKey == null)
+        {
+            Console.WriteLine("Telegram bot api key didn't set");
+            return;
+        }
+        _openAiApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+        if (_openAiApiKey == null)
+        {
+            Console.WriteLine("OpenAI api key didn't set");
+            return;
+        }
+
+        string? allowedUsers = Environment.GetEnvironmentVariable("ALLOWED_USERS");
+        if (allowedUsers == null)
+        {
+            Console.WriteLine("List of allowed users didn't set");
+            return;
+        }
+
+        _allowedUsersList = allowedUsers.Split(' ').Select(Int32.Parse).ToList();
+
+        TelegramBotClient client = new(telegramBotApiKey);
+        client.StartReceiving(UpdateHandler, ErrorHandler);
+
+        _tikToken = TikToken.EncodingForModel("gpt-3.5-turbo");
+
+        Console.ReadLine();    
+    }
+
+    private static async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        Message? message = update.Message;
+        if (message == null)
+        {
+            return;
+        }
+
+        int messageId = message.MessageId;
+        long chatId = message.Chat.Id;
+        
+        string? text = message.Text;
+        if (text == null)
+        {
+            return;
+        }
+        
+        try
+        {
+            if (_allowedUsersList != null && _allowedUsersList.Contains((int) chatId) == false)
+            {
+                await client.SendTextMessageAsync(chatId,
+                    "You don't have privileges to use this bot.",
+                    replyToMessageId: messageId,
+                    cancellationToken: token);
+                return;
+            }
+            
+            // message is URL?
+            UrlExtractor urlExtractor = new();
+            string? url = urlExtractor.ExtractUrl(text);
+            if (url == null)
+            {
+                await client.SendTextMessageAsync(chatId,
+                    "URL didn't found.",
+                    replyToMessageId: messageId,
+                    cancellationToken: token);
+                return;
+            }
+
+            TextContentExtractor contentExtractor = new(_openAiApiKey!);
+            string content = await contentExtractor.ExtractTextContentFromUrlAsync(url);
+
+            List<Sentence> sentences = TokenizerAndSentenceExtractor.GetSentencesList(content);
+            Fraction mainFraction = new(sentences);
+
+            List<Fraction> fractions = mainFraction.SplitIntoSubfractions(Limit);
+
+            List<string> responses = new();
+            foreach (Fraction fraction in fractions)
+            {
+                string paraphrasedResponse = await GetParaphrasedResponse(fraction);
+                responses.Add(paraphrasedResponse);
+                
+                await client.SendTextMessageAsync(chatId,
+                    paraphrasedResponse,
+                    replyToMessageId: messageId,
+                    cancellationToken: token);
+            }
+
+            List<Sentence> responseSentences = responses.Select(s => s.Replace("'", "")
+                    .Replace("\"", ""))
+                .SelectMany(TokenizerAndSentenceExtractor.GetSentencesList)
+                .ToList();
+
+            string tempAudioFolder = Path.Combine(PiperFolderPath, Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempAudioFolder);
+
+            for (int i = 0; i < responseSentences.Count; i++)
+            {
+                try
+                {
+                    string audioPath = Path.Combine(tempAudioFolder, $"{i}.wav");
+                    Command cmd = Cli.Wrap("/bin/bash")
+                        .WithWorkingDirectory(PiperFolderPath)
+                        .WithArguments($"-c \"echo '{responseSentences[i]}' | ./piper -m 'en-us-ryan-high.onnx' --output_file '{audioPath}'\"");
+                    await cmd.ExecuteBufferedAsync();
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+            
+            WavFinder finder = new(tempAudioFolder);
+            
+            // Sorting the list by file name (as integer)
+            string[] wavFiles = finder.FindWavFiles().OrderBy(path =>
+            {
+                string fileName = Path.GetFileNameWithoutExtension(path);
+                return int.Parse(fileName);
+            }).ToArray();
+
+            string outputFile = Path.Combine(tempAudioFolder, OutputFileName);
+            Combine(outputFile, wavFiles);
+            
+            await client.SendAudioAsync(chatId,
+                InputFile.FromStream(File.OpenRead(outputFile)),
+                replyToMessageId: messageId,
+                cancellationToken: token);
+            
+            Directory.Delete(tempAudioFolder, true);
+        }
+        catch (Exception)
+        {
+            await client.SendTextMessageAsync(chatId,
+                "Error. Something went wrong",
+                replyToMessageId: messageId,
+                cancellationToken: token);
+        }
+    }
+
+    private static async Task<string> GetParaphrasedResponse(Fraction fraction)
+    {
+        OpenAIAPI api = new(_openAiApiKey);
+        Conversation? chat = api.Chat.CreateConversation();
+        chat.AppendSystemMessage(SystemMessage);
+        string request = RequestManager.GetRequestForSentences(_tikToken!, fraction.Sentences);
+        chat.AppendUserInput(request);
+        string response = await chat.GetResponseFromChatbotAsync();
+        return response;
+    }
+
+    private static Task ErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+    {
+        Console.WriteLine($"Error. Something went wrong:\n{exception}");
+        return Task.CompletedTask;
+    }
+    
+    public static void Combine(string outputFile, params string[] inputFiles)
+    {
+        byte[] buffer = new byte[1024];
+        WaveFileWriter? waveFileWriter = null;
+
+        try
+        {
+            foreach (string sourceFile in inputFiles)
+            {
+                using WaveFileReader reader = new(sourceFile);
+                if (waveFileWriter == null)
+                {
+                    // first time in create new Writer
+                    waveFileWriter = new WaveFileWriter(outputFile, reader.WaveFormat);
+                }
+                else
+                {
+                    if (!reader.WaveFormat.Equals(waveFileWriter.WaveFormat))
+                    {
+                        throw new InvalidOperationException("Can't concatenate WAV Files that don't share the same format");
+                    }
+                }
+
+                int read;
+                while ((read = reader.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    waveFileWriter.Write(buffer, 0, read);
+                }
+            }
+        }
+        finally
+        {
+            if (waveFileWriter != null)
+            {
+                waveFileWriter.Dispose();
+            }
+        }
+    }
+}
+
+public class WavFinder
+{
+    private readonly string _dir;
+
+    public WavFinder(string dir)
+    {
+        _dir = dir;
+    }
+
+    public string[] FindWavFiles()
+    {
+        return Directory.EnumerateFiles(_dir, "*.wav", SearchOption.AllDirectories).ToArray();
+    }
+}
--- Reader/Models/Fraction.cs
+++ Reader/Models/Fraction.cs
@@ -4,6 +4,8 @@ namespace Reader.Models;
 
 public class Fraction
 {
+    private readonly TikToken _tikToken = TikToken.EncodingForModel("gpt-3.5-turbo");
+
     public Fraction(List<Sentence> sentences)
     {
         Sentences = sentences;
@@ -13,7 +15,47 @@ public class Fraction
 
     public bool IsSatisfiesLimit(int limit)
     {
-        TikToken tikToken = TikToken.EncodingForModel("gpt-3.5-turbo");
-        return tikToken.Encode(ToString()).Count < limit;
+        return _tikToken.Encode(ToString()).Count < limit;
+    }
+    
+    public List<Fraction> SplitIntoSubfractions(int limit)
+    {
+        List<Fraction> subFractions = new();
+        List<Sentence> currentSentences = new();
+
+        foreach (Sentence sentence in Sentences)
+        {
+            // Check if a single sentence exceeds the limit
+            Fraction singleSentenceFraction = new(new List<Sentence> { sentence });
+            if (!singleSentenceFraction.IsSatisfiesLimit(limit))
+            {
+                throw new InvalidOperationException("A single sentence exceeds the given limit.");
+            }
+
+            currentSentences.Add(sentence);
+            Fraction currentFraction = new(currentSentences);
+
+            if (currentFraction.IsSatisfiesLimit(limit))
+            {
+                continue;
+            }
+
+            currentSentences.Remove(sentence); // remove last sentence, it exceeded the limit
+                
+            // add previously collected sentences as a new Fraction
+            subFractions.Add(new Fraction(new List<Sentence>(currentSentences)));
+                
+            // start new Fraction with the current sentence
+            currentSentences.Clear();
+            currentSentences.Add(sentence);
+        }
+
+        // add remaining sentences as the last Fraction
+        if (currentSentences.Count > 0)
+        {
+            subFractions.Add(new Fraction(currentSentences));
+        }
+
+        return subFractions;
     }
 }
