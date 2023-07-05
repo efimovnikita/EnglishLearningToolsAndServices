28b696a9611a03f9c4e8ce42373cbf9251ce2ef4
maskedball <maskedballmail@gmail.com>
2023-07-05 17:12:32 +0300

    Add IP logging to TextToSpeech requests and related tasks
    The updated program.cs now includes logging IP addresses for all incoming TextToSpeech requests and their corresponding tasks. This information will aid in both request tracking and debugging by providing origin details for each request. This was achieved by integrating IHttpContextAccessor in the mapped post function for "/TextToSpeech" in order to access HttpContext and extract the IP address. Subsequently, this address is now logged during various stages of the request such as text sanitation, audio generation, audio file reading, etc.

--- TTSApi/Program.cs
+++ TTSApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Reflection;
 using CliWrap;
 using CliWrap.Buffered;
@@ -8,6 +9,7 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen(); 
 builder.Services.AddCors();
+builder.Services.AddHttpContextAccessor();
 
 builder.WebHost.UseUrls("http://*:5000");
 
@@ -19,13 +21,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapPost("/TextToSpeech", async (TextRequest request, IWebHostEnvironment _, ILogger<Program> logger) =>
+app.MapPost("/TextToSpeech", async (TextRequest request, IWebHostEnvironment _, ILogger<Program> logger, 
+    IHttpContextAccessor accessor) =>
 {
-    logger.LogInformation("Received a new TextToSpeech request");
+    IPAddress? ipAddress = accessor.HttpContext!.Connection.RemoteIpAddress;
+
+    logger.LogInformation("Received a new TextToSpeech request from IP: {IP}", ipAddress);
     
    if (String.IsNullOrEmpty(request.Text))
    {
-       logger.LogWarning("Received request with empty text");
+       logger.LogWarning("Received request with empty text from IP: {IP}", ipAddress);
        return Results.BadRequest("Text is required.");
    }
 
@@ -33,8 +38,8 @@ app.MapPost("/TextToSpeech", async (TextRequest request, IWebHostEnvironment _, 
    string outputFilePath = Path.Combine(location, "output.wav");
 
    // Sanitize the input text
-   string sanitizedText = System.Net.WebUtility.HtmlEncode(request.Text);
-   logger.LogInformation("Text has been successfully sanitized");
+   string sanitizedText = WebUtility.HtmlEncode(request.Text);
+   logger.LogInformation("Text from {IP} has been successfully sanitized", ipAddress);
 
    try
    {
@@ -43,12 +48,12 @@ app.MapPost("/TextToSpeech", async (TextRequest request, IWebHostEnvironment _, 
            .WithArguments(
                $"-c \"echo '{sanitizedText}' | ./piper -m en-us-amy-low.onnx --output_file {outputFilePath}\"");
        await cmd.ExecuteBufferedAsync();
-       logger.LogInformation("Successfully generated audio at {Path}", outputFilePath);
+       logger.LogInformation("Successfully generated audio at {Path} for IP: {IP}", outputFilePath, ipAddress);
    }
    catch (Exception exception)
    {
        // Log the error for debug purposes
-       logger.LogError(exception, "An error occurred while generating the audio");
+       logger.LogError(exception, "An error occurred while generating the audio for IP: {IP}", ipAddress);
        return Results.Problem("An error occurred while generating the audio.");
    }
 
@@ -59,12 +64,12 @@ app.MapPost("/TextToSpeech", async (TextRequest request, IWebHostEnvironment _, 
        ILogger<DeleteOnCloseStream> deleteOnCloseStreamLogger =
            app.Services.GetRequiredService<ILogger<DeleteOnCloseStream>>();
        fileStream = new DeleteOnCloseStream(deleteOnCloseStreamLogger, outputFilePath, FileMode.Open);
-       logger.LogInformation("Successfully opened the generated audio file");
+       logger.LogInformation("Successfully opened the generated audio file for IP: {IP}", ipAddress);
    }
    catch (Exception e)
    {
        // Log the error for debug purposes
-       logger.LogError(e, "An error occurred while reading the audio file");
+       logger.LogError(e, "An error occurred while reading the audio file for {IP}", ipAddress);
        return Results.Problem("An error occurred while reading the audio file.");
    }
 
