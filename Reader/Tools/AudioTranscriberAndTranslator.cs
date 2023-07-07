afe1752764063ceb8a8d4f09ee2aa2c44dbd3ff2
maskedball <maskedballmail@gmail.com>
2023-07-07 11:35:28 +0300

    Add automatic audio translation feature
    The current implementation of audio transcription has been extended to now support automatic translation of non-English videos. Changes were made in the TextContentExtractor.cs to check for an auto-generated track info and then choose either to transcribe or translate based on the language code. The AudioTranscriber class was renamed to AudioTranscriberAndTranslator and extended to include audio translation functionality. Corresponding changes related to this rename were also made in Index.razor file. This feature is important to improve accessibility and inclusivity of the application.

--- Reader/Tools/AudioTranscriberAndTranslator.cs
+++ Reader/Tools/AudioTranscriberAndTranslator.cs
@@ -3,11 +3,11 @@ using Newtonsoft.Json.Linq;
 
 namespace Reader.Tools;
 
-public class AudioTranscriber
+public class AudioTranscriberAndTranslator
 {
     private readonly HttpClient _httpClient;
 
-    public AudioTranscriber(string openAiKey)
+    public AudioTranscriberAndTranslator(string openAiKey)
     {
         _httpClient = new HttpClient();
         _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {openAiKey}");
@@ -15,17 +15,28 @@ public class AudioTranscriber
 
     public async Task<string> TranscribeAudio(string fileName)
     {
+        return await GetText(fileName, "https://api.openai.com/v1/audio/transcriptions");
+    }
+    
+    public async Task<string> TranslateAudio(string fileName)
+    {
+        return await GetText(fileName, "https://api.openai.com/v1/audio/translations");
+    }
+
+    private async Task<string> GetText(string fileName, string endpointUrl)
+    {
         ByteArrayContent byteArrayContent = new(File.ReadAllBytes(fileName));
         byteArrayContent.Headers.Remove("Content-Type");
         byteArrayContent.Headers.TryAddWithoutValidation("Content-Type", "application/octet-stream");
-        
+
         StringContent modelContent = new("whisper-1");
 
         using MultipartFormDataContent formData = new();
         formData.Add(byteArrayContent, "file", Path.GetFileName(fileName));
         formData.Add(modelContent, "model");
 
-        HttpResponseMessage response = await _httpClient.PostAsync("https://api.openai.com/v1/audio/transcriptions", formData);
+        HttpResponseMessage response =
+            await _httpClient.PostAsync(endpointUrl, formData);
         response.EnsureSuccessStatusCode();
         string responseString = await response.Content.ReadAsStringAsync();
 
@@ -35,7 +46,8 @@ public class AudioTranscriber
             throw new Exception("Error when trying to get transcript from audio");
         }
 
-        string text = jsonResponse["text"]!.Value<string>() ?? "";
+        string text = (jsonResponse["text"] ?? throw new Exception("Error when trying to get transcript from audio"))
+            .Value<string>() ?? "";
 
         return text;
     }
