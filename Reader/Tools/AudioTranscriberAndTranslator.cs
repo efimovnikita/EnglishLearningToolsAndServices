ba00e088dffc9dda13a9ab9c356eca3e0bfc8c67
maskedball <maskedballmail@gmail.com>
2023-10-26 18:12:34 +0300

    Added support for much longer audio files.

--- Reader/Tools/AudioTranscriberAndTranslator.cs
+++ Reader/Tools/AudioTranscriberAndTranslator.cs
@@ -1,3 +1,7 @@
+using System.Reflection;
+using System.Text;
+using CliWrap;
+using CliWrap.Buffered;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -25,30 +29,71 @@ public class AudioTranscriberAndTranslator
 
     private async Task<string> GetText(string fileName, string endpointUrl)
     {
-        ByteArrayContent byteArrayContent = new(File.ReadAllBytes(fileName));
-        byteArrayContent.Headers.Remove("Content-Type");
-        byteArrayContent.Headers.TryAddWithoutValidation("Content-Type", "application/octet-stream");
+        StringBuilder stringBuilder = new();
+        List<string> tempParts = new();
 
-        StringContent modelContent = new("whisper-1");
-
-        using MultipartFormDataContent formData = new();
-        formData.Add(byteArrayContent, "file", Path.GetFileName(fileName));
-        formData.Add(modelContent, "model");
-
-        HttpResponseMessage response =
-            await _httpClient.PostAsync(endpointUrl, formData);
-        response.EnsureSuccessStatusCode();
-        string responseString = await response.Content.ReadAsStringAsync();
-
-        JObject? jsonResponse = JsonConvert.DeserializeObject<JObject>(responseString);
-        if (jsonResponse == null)
+        try
         {
-            throw new Exception("Error when trying to get transcript from audio");
+            string? assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // Get the duration of the audio file in seconds
+            BufferedCommandResult durationProcess = await Cli.Wrap("ffprobe")
+                .WithArguments(
+                    $"-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {fileName}")
+                .ExecuteBufferedAsync();
+
+            Double.TryParse(durationProcess.StandardOutput, out double duration);
+            
+            // Here we calculate how many segments based on fixed chunk duration
+            const int chunkDuration = 600; // in seconds. Adjust if needed.
+            int totalChunks = (int) Math.Ceiling(duration / chunkDuration);
+
+            for (int i = 0; i < totalChunks; i++)
+            {
+                if (assemblyLocation == null)
+                {
+                    continue;
+                }
+
+                string partFileName = Path.Combine(assemblyLocation, $"output{i}.mp3");
+                tempParts.Add(partFileName);
+
+                await Cli.Wrap("ffmpeg")
+                    .WithArguments($"-y -i {fileName} -ss {i * chunkDuration} -t {chunkDuration} {partFileName}")
+                    .ExecuteAsync();
+
+                if (!File.Exists(partFileName))
+                {
+                    continue;
+                }
+
+                ByteArrayContent byteArrayContent = new(File.ReadAllBytes(partFileName));
+                byteArrayContent.Headers.Remove("Content-Type");
+                byteArrayContent.Headers.TryAddWithoutValidation("Content-Type", "application/octet-stream");
+
+                StringContent modelContent = new("whisper-1");
+
+                using MultipartFormDataContent formData = new();
+                formData.Add(byteArrayContent, "file", Path.GetFileName(partFileName));
+                formData.Add(modelContent, "model");
+
+                HttpResponseMessage response = await _httpClient.PostAsync(endpointUrl, formData);
+                response.EnsureSuccessStatusCode();
+
+                string responseString = await response.Content.ReadAsStringAsync();
+                JObject? jsonResponse = JsonConvert.DeserializeObject<JObject>(responseString);
+                string text = jsonResponse?["text"]?.Value<string>() ?? "";
+
+                stringBuilder.Append(text);
+                File.Delete(partFileName);
+            }
+
+            return stringBuilder.ToString();
         }
-
-        string text = (jsonResponse["text"] ?? throw new Exception("Error when trying to get transcript from audio"))
-            .Value<string>() ?? "";
-
-        return text;
+        catch
+        {
+            tempParts.Where(File.Exists).ToList().ForEach(File.Delete);
+            throw;
+        }
     }
 }
