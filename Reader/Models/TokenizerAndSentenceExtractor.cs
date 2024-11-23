bf98d5a7cbbcab1980a9411183e1b9416fe33478
Nikita Efimov <maskedballmail@gmail.com>
2024-11-23 14:57:24 +0300

    added the italian language

--- Reader/Models/TokenizerAndSentenceExtractor.cs
+++ Reader/Models/TokenizerAndSentenceExtractor.cs
@@ -1,3 +1,4 @@
+using opennlp.tools.langdetect;
 using opennlp.tools.sentdetect;
 using opennlp.tools.tokenize;
 
@@ -7,8 +8,45 @@ public class TokenizerAndSentenceExtractor
 {
     public static List<Sentence> GetSentencesList(string input)
     {
-        java.io.InputStream sentenceModelIn = new java.io.FileInputStream("opennlp-en-ud-ewt-sentence-1.0-1.9.3.bin");
-        java.io.InputStream tokensModelIn = new java.io.FileInputStream("opennlp-en-ud-ewt-tokens-1.0-1.9.3.bin");
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new ArgumentException("Input text cannot be null or empty");
+        }
+
+        // Detect language first
+        var langDetectModelIn = new java.io.FileInputStream("langdetect-183.bin");
+        var languageDetector = new LanguageDetectorModel(langDetectModelIn);
+        var languageDetectorMe = new LanguageDetectorME(languageDetector);
+        var language = languageDetectorMe.predictLanguage(input);
+        var lang = language.getLang();
+
+        if (string.IsNullOrEmpty(lang))
+        {
+            throw new InvalidOperationException("Could not detect language");
+        }
+
+        // Select appropriate model files based on language
+        string sentenceModelPath;
+        string tokenizerModelPath;
+
+        switch (lang.ToLower())
+        {
+            case "eng":
+                sentenceModelPath = "opennlp-en-ud-ewt-sentence-1.0-1.9.3.bin";
+                tokenizerModelPath = "opennlp-en-ud-ewt-tokens-1.0-1.9.3.bin";
+                break;
+            case "ita":
+                sentenceModelPath = "opennlp-it-ud-vit-sentence-1.0-1.9.3.bin";
+                tokenizerModelPath = "opennlp-it-ud-vit-tokens-1.0-1.9.3.bin";
+                break;
+            default:
+                throw new NotSupportedException($"Language '{lang}' is not supported");
+        }
+
+        // Load appropriate models
+        java.io.InputStream sentenceModelIn = new java.io.FileInputStream(sentenceModelPath);
+        java.io.InputStream tokensModelIn = new java.io.FileInputStream(tokenizerModelPath);
+        
         SentenceModel sentenceModel = new(sentenceModelIn);
         TokenizerModel tokenizerModel = new(tokensModelIn);
         SentenceDetectorME sentenceDetector = new(sentenceModel);
